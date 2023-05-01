d99e3ec1f95278f47712a9d7c4e0fe5d557cca5b
Horizon1026 <lijiapeng_atlantis@163.com>
2023-05-01 16:33:36 +0800

    slam solver

--- src/general_graph_optimizor/marginalizor/marginalizor.h
+++ src/general_graph_optimizor/marginalizor/marginalizor.h
@@ -7,7 +7,54 @@
 
 namespace SLAM_SOLVER {
 
+template <typename Scalar>
+struct MargOptions {
 
+};
+
+/* Class Marginalizor Declaration. */
+template <typename Scalar>
+class Marginalizor {
+
+public:
+    Marginalizor() = default;
+    virtual ~Marginalizor() = default;
+
+    // Marginalize graph optimization problem.
+    bool Marginalize(bool use_prior = true);
+
+    // Sort vertices to be marged to the front or back of vertices vector.
+    // Keep the other vertices the same order.
+    bool SortVerticesToBeMargedToTheFront(const std::vector<Vertex<Scalar> *> &vertices);
+    bool SortVerticesToBeMargedToTheBack(const std::vector<Vertex<Scalar> *> &vertices);
+
+    // Construct information.
+    virtual void ConstructInformation(bool use_prior = true) = 0;
+
+    // Create prior information.
+    virtual void CreatePriorInformation() = 0;
+
+private:
+    // General options for marginalizor.
+    MargOptions<Scalar> options_;
+
+    // The graph optimization problem to be marged.
+    Graph<Scalar> *problem_ = nullptr;
+
+};
+
+/* Class Marginalizor Definition. */
+template <typename Scalar>
+bool Marginalizor<Scalar>::Marginalize(bool use_prior) {
+    if (problem_ == nullptr) {
+        return false;
+    }
+
+    // Sort all vertices, determine their location in incremental function.
+    problem_->SortVertices(false);
+
+    return true;
+}
 
 }
 
