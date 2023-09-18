c86cb7b9169927584367c3f5b28bef1bea4adac2
Horizon1026 <lijiapeng_atlantis@163.com>
2023-09-19 00:04:51 +0800

    update code

--- test/test_general_graph_optimizor_norm_plane_BA.cpp
+++ test/test_general_graph_optimizor_norm_plane_BA.cpp
@@ -110,25 +110,8 @@ int main(int argc, char **argv) {
     std::vector<TVec3<Scalar>> points;
     GenerateSimulationData(cameras, points);
 
-    // Generate vertex of cameras and points.
-    std::array<std::unique_ptr<Vertex<Scalar>>, kCameraFrameNumber> all_camera_pos = {};
-    std::array<std::unique_ptr<VertexQuat<Scalar>>, kCameraFrameNumber> all_camera_rot = {};
-    for (int32_t i = 0; i < kCameraFrameNumber; ++i) {
-        all_camera_pos[i] = std::make_unique<Vertex<Scalar>>(3, 3);
-        all_camera_pos[i]->param() = cameras[i].p_wc;
-        all_camera_rot[i] = std::make_unique<VertexQuat<Scalar>>(4, 3);
-        all_camera_rot[i]->param() << cameras[i].q_wc.w(), cameras[i].q_wc.x(), cameras[i].q_wc.y(), cameras[i].q_wc.z();
-
-        if (i > 2) {
-            all_camera_pos[i]->param() += TVec3<Scalar>(0.2, 0.2, 0);
-        }
-    }
-
-    std::array<std::unique_ptr<Vertex<Scalar>>, kPointsNumber> all_points = {};
-    for (int32_t i = 0; i < kPointsNumber; ++i) {
-        all_points[i] = std::make_unique<Vertex<Scalar>>(3, 3);
-        all_points[i]->param() = points[i];
-    }
+    // Use include here is interesting.
+    #include "embeded_add_vertices_of_BA.h"
 
     // Generate edges between cameras and points.
     std::array<std::unique_ptr<EdgeReproject<Scalar>>, kCameraFrameNumber * kPointsNumber> reprojection_edges = {};
