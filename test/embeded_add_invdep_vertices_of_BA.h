b4df1de15412e513a8794457d1f1be7543f17419
Horizon1026 <lijiapeng_atlantis@163.com>
2023-12-31 19:02:02 +0800

    update code

--- test/embeded_add_invdep_vertices_of_BA.h
+++ test/embeded_add_invdep_vertices_of_BA.h
@@ -4,8 +4,10 @@ std::array<std::unique_ptr<VertexQuat<Scalar>>, kCameraFrameNumber> all_camera_r
 for (int32_t i = 0; i < kCameraFrameNumber; ++i) {
     all_camera_pos[i] = std::make_unique<Vertex<Scalar>>(3, 3);
     all_camera_pos[i]->param() = cameras[i].p_wc;
+    all_camera_pos[i]->name() = std::string("p_wc") + std::to_string(i);
     all_camera_rot[i] = std::make_unique<VertexQuat<Scalar>>(4, 3);
     all_camera_rot[i]->param() << cameras[i].q_wc.w(), cameras[i].q_wc.x(), cameras[i].q_wc.y(), cameras[i].q_wc.z();
+    all_camera_rot[i]->name() = std::string("q_wc") + std::to_string(i);
 
     if (i > 2) {
         all_camera_pos[i]->param() += TVec3<Scalar>(0.5, 0.5, 0.5);
